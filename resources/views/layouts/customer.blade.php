a9ebe41a9047a7a69d58300dff30d9cc2a040707
Marcos Soares <mrlsoares@gmail.com>
2016-02-01 13:11:29 -0200

    atualizado extends blade de customer  para layouts.app

--- resources/views/layouts/customer.blade.php
+++ resources/views/layouts/customer.blade.php
@@ -47,7 +47,7 @@
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
-                <li><a href="{{ url('/customer') }}">Home</a></li>
+                <li><a href="{{ url('/customer/order') }}">Meus Pedidos</a></li>
 
 
 
